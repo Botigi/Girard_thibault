a5d1ff5cc67fd3802cf57a5c1921ca561c4caf49
thibault <botigirard@gmail.com>
2025-04-02 12:43:16 +0200

    Update générateur_blague.js

--- générateur_blague.js
+++ générateur_blague.js
@@ -1,12 +1,12 @@
 async function chargerBlague() {
     try {
-        const reponse = await fetch('https://api.chucknorris.io/jokes/random');
+        const reponse = await fetch('https://api.chucknorris.io/jokes/random');  // chucknorris le goat ultime
         const data = await reponse.json();
         document.getElementById('blague').innerText = data.value;
     } catch (erreur) {
         document.getElementById('blague').innerText = "Erreur de chargement de la blague";
     }
-}
+} // car la vie snas blague est moins drole :(
 
 // Charger une blague au démarrage
 chargerBlague();
